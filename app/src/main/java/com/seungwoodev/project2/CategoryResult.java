cf419e4544b2e8838baaf301340c38914d249bb5
kyung <kyungha1227@naver.com>
2021-07-12 12:16:14 +0900

    expanded recyclerview + fab

---
+++ app/src/main/java/com/seungwoodev/project2/CategoryResult.java
@@ -0,0 +1,13 @@
+package com.seungwoodev.project2;
+
+import java.util.ArrayList;
+
+public class CategoryResult {
+    private Integer code;
+    private ArrayList<ArrayList<String>> name;
+
+    public Integer getCode(){return code;}
+    public ArrayList<ArrayList<String>> getName() {
+        return name;
+    }
+}
